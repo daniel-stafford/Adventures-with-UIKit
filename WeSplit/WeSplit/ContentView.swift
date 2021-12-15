b559863304d632732730bff1ef6b9ee0167863fe
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-15 22:21:39 +0100

    Finish project 1, part one

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -2,26 +2,71 @@
 //  ContentView.swift
 //  WeSplit
 //
-//  Created by Daniel Stafford on 12/14/21.
+//  Created by Daniel Stafford on 12/15/21.
 //
 
 import SwiftUI
 
-//. View comes from SwiftUI, and is the basic protocol that must be adopted by anything you want to draw on the screen – all text, buttons, images, and more are all views, including your own layouts that combine other views.
-//The View protocol has only one requirement, which is that you have a computed property called body that returns some View.
+//struct ContentView: View {
+//    let pets = ["Cat", "Fish", "Dog"]
+//    @State private var selectedPet = "Dog"
+////     @State private var dob = ""
+//    //  Cant use mutating as this is a property. @State, a property wrapper,eeeeeeeee is specifically designed for simple properties that are stored in one view.
+////    @State private var tapCount = 0
+//
+  
 struct ContentView: View {
-    //var body: some View < - defines a new computed property called body with type some View. This means it will return something that conforms to the View protocol, which is our layout. Behind the scenes this will actually result in a very complicated data type being returned based on all the things in our layout, but some View means we don’t need to worry about that.
+    let pets = ["Dog", "Cat", "Fish"]
+    @State private var selectedPet = "Dog"
+
     var body: some View {
-        //Text views are simple pieces of static text that get drawn onto the screen, and will automatically wrap across multiple lines as needed.
-        Text("This is a change")
-        //This is what Swift calls a modifier, which are regular methods with one small difference: they always return a new view that contains both your original data, plus the extra modification you asked for. In our case that means body will return a padded text view, not just a regular text view.
-            .padding()
+        NavigationView {
+            Form {
+                Picker("Select your pet", selection: $selectedPet){
+                    //\.self <- means “the strings themselves are uniqu
+                    ForEach(pets, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    
+//        NavigationView {
+//            Form {
+//                //when you see a dollar sign before a property name, it creates a two-way binding: the value of the property is read, but also written.
+//                Section {
+//                    TextField("s", text: $dob)
+//                }
+//                Section {
+//                    Text("Your birthday is\(dob)")
+//                }
+//            }
+//            .navigationTitle("Binding state to user interface controls")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
+        
+//        Button("Tap Count: \(tapCount)") {
+//            tapCount += 1
+//        }
+        
+        //give a title to a page
+//        NavigationView {
+//            Form {
+//                //can use groups (10+ items) or sections in a form
+//                Section {
+//                    Text("This is a form")
+//                }
+//            }
+//            //navigationTitle belongs to form
+//            .navigationTitle("SwiftUI")
+//            //more compact title
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
+//    }
     }
 }
 
-//. This piece of code won’t actually form part of your final app that goes to the App Store, but is instead specifically for Xcode to use so it can show a preview of your UI design alongside your code.
-//You can customize the preview code if you want, and they will only affect the way the canvas shows your layouts – it won’t change the actual app that gets run.
-// 👀 Option+Cmd+P does the same as clicking Resume.
+//this is used for canvas but not used when deployed
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
