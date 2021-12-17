ed95d81c4cc3695eaf63e2e4ed01ec8fcc634674
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-17 13:32:57 +0100

    Finish alert section

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,42 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isAlertShown = false
+    
     var body: some View {
-        VStack {
-            Button("Button 1", role: .destructive) {}
-            .buttonStyle(.bordered)
-            Button("Button 2") {}
-            .buttonStyle(.bordered)
-            //Important: Apple explicitly recommends against using too many prominent buttons, because when everything is prominent nothing is.
-            Button("Button 3") {}
-            .buttonStyle(.borderedProminent)
-            .tint(.mint)
-            Button("Delete Section", role: .destructive, action: runDelete)
-            //custom, button This is particularly common when you want to incorporate images into your buttons.
-            Button {
-                print("Button was tapped")
-            } label: {
-                Text("Tap Me")
-                    .padding()
-                    .foregroundStyle(.white)
-                    .background(.red)
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Image(systemName: "pencil")
-            }
-            Button {
-                print("Edit button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-            }
-            //Tip: If you find that your images have become filled in with a color, for example showing as solid blue rather than your actual picture, this is probably SwiftUI coloring them to show that they are tappable. To fix the problem, use the renderingMode(.original) modifier to force SwiftUI to show the original image rather than the recolored version.
-            Image(systemName: "pencil").renderingMode(.original)
+        Button("Show Alert"){
+            isAlertShown = true
         }
-    }
-    func runDelete(){
-        print("I am deleting")
+        .alert("This is the alert header", isPresented: $isAlertShown){
+//            That’s an empty closure, meaning that we aren’t assigning any functionality to run when the button is pressed. That doesn’t matter, though, because any button inside an alert will automatically dismiss the alert – that closure is there to let us add any extra functionality beyond just dismissing the alert.
+//            Button("Thanks"){}
+            Button("Delete", role:.destructive){}
+            Button("Ceancel", role: .cancel){}
+        } message: {
+            Text("This is the alert text")
+        }
     }
 }
 
