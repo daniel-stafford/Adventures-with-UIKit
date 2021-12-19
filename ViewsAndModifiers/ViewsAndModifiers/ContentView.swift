98b8432e18bd9d050ffa2d0afe62bba5dd7e99ed
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-19 13:52:56 +0100

    Add Custom modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,29 +7,59 @@
 
 import SwiftUI
 
-//View composition
-struct CapsuleText: View {
-    //prop to be passed
-    var text: String
 
-    var body: some View {
-        Text(text)
+
+// Custom modifiers
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .font(.largeTitle)
+            .foregroundColor(.white)
             .padding()
             .background(.blue)
-            .clipShape(Capsule())
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+//add extension to make use of custom modifier cleaner
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+//Tip: Often folks wonder when it’s better to add a custom view modifier versus just adding a new method extension to View, and really it comes down to one main reason: custom view modifiers can have their own stored properties, whereas extensions to View cannot.
+struct Watermark: ViewModifier {
+    //new struct ViewModifier can take a parameter
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
         VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                .foregroundColor(.white)
-            CapsuleText(text: "Second")
-                .foregroundColor(.yellow)
-            CapsuleText(text: "Third")
-                .foregroundColor(.red)
+            Text("custom modifier")
+                .modifier(Title())
+            Text("custom modifier with extension").titleStyle()
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "Modifier that creates a new view structure")
         }
     }
 }
