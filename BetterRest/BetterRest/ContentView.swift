bcf28f3624b61f4d5152853fad084435639e36c7
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-20 12:06:24 +0100

    Add Entering numbers with Stepper

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
     }
 }
 
