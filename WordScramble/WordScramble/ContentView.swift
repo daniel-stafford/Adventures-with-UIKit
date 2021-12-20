1dd31a7dbd043e06ae7e470fe650065edae3daa7
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-20 22:31:32 +0100

    Add Working with strings, spellchecking

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,15 +11,39 @@ struct ContentView: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
 
     var body: some View {
-        Text("Loading Assets")
+        Button("Press me") {
+            getText()
+        }
     }
 
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we laded the file into the string
-            }
-        }
+    func getText() {
+        //chopping up strings, trimming, etc.
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ")
+        print(letters)
+        let input2 = """
+        a
+        b
+        c
+        """
+        let letters2 = input2.components(separatedBy: "\n")
+        print(letters2)
+        let letter = letters2.randomElement() ?? "a"
+        let trimmed = letter.trimmingCharacters(in: .whitespacesAndNewlines)
+        print(trimmed)
+        
+        //spell check
+        let word = "swift"
+        let checker = UITextChecker()
+        print(checker)
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        print(range)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        print(misspelledRange)
+        // NSNotFound is an empty optional i.e. no spelling mistakes
+        let allGood = misspelledRange.location == NSNotFound
+        print(allGood)
     }
 }
 
