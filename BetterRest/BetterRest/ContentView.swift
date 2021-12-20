7677c95d492a1edf6620a8efdd4e638ee36eaabe
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-20 15:12:34 +0100

    Add working with dates 😩

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,31 +11,33 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4 ... 12, step: 0.25)
-        // Now, you might think that label looks ugly, and try replacing it with this:
-        DatePicker("Please enter a date", selection: $wakeUp)
-        // But if you do that you now have two problems: the date picker still makes space for a label even though it’s empty, and now users with the screen reader active (more familiar to us as VoiceOver) won’t have any idea what the date picker is for.
-        DatePicker("", selection: $wakeUp)
-        // That still includes the original label so screen readers can use it for VoiceOver, but now they aren’t visible onscreen any more – the date picker won’t be pushed to one side by some empty text.
-        DatePicker("Please enter a date", selection: $wakeUp)
-            .labelsHidden()
-        // Date pickers provide us with a couple of configuration options that control how they work. First, we can use displayedComponents to decide what kind of options users should see:
-        //        If you don’t provide this parameter, users see a day, hour, and minute.
-        //        If you use .date users see month, day, and year.
-        //        If you use .hourAndMinute users see just the hour and minute components.
-        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-        ///  //Finally, there’s an in parameter that works just the same as with Stepper: we can provide it with a date range, and the date picker will ensure the user can’t select beyond it.
-        /// One side range <- Date.now...>
-        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now... , displayedComponents: .date)
+        Text("Dates are hard!")
     }
 
-    //    Now, we’ve been using ranges for a while now, and you’re used to seeing things like 1...5 or 0..<10, but we can also use Swift dates with ranges. For example:
     func exampleDates() {
         // create a second Date instance set to one day in seconds from now
+        // issue with tomorrow is that not all days have 86400 seconds
+        // hence use iOS' Date API
         let tomorrow = Date.now.addingTimeInterval(86400)
-        // create a range from those two
-        let range = Date.now ... tomorrow
+        var components = DateComponents()
+        // we want to say “give me an 8am wake up time, regardless of what day it is today.”
+        components.hour = 8
+        components.minute = 0
+        let date = Calendar.current.date(from: components) ?? Date.now
+        // The second challenge is how we could read the hour they want to wake up. Remember, DatePicker is bound to a Date giving us lots of information, so we need to find a way to pull out just the hour and minute components.
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+       // we still need to unwrap the optionals or provide default values.
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        // using format
+        //get time from a date
+        Text(Date.now, format: .dateTime.hour().minute())
+        //get day, month, year, from date
+        Text(Date.now, format: .dateTime.day().month().year())
+        //use format directly
+        Text(Date.now.formatted(date: .long, time: .shortened))
     }
+     
 }
 
 struct ContentView_Previews: PreviewProvider {
