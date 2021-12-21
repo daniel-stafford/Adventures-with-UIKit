3acb0fe6c23e0d899f8a8f2bcc1f3d6069b1873f
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-21 15:09:53 +0100

    Add game logic

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
 
     var body: some View {
         NavigationView {
@@ -32,7 +35,13 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            // run when app renders
             .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
 
@@ -43,12 +52,62 @@ struct ContentView: View {
         // exit if the remaining string is empty
         guard answer.count > 0 else { return }
 
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
         // very simople animation
         withAnimation {
             // insert at 0 rather than append to avoid new words running off screen
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+    }
+
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    func isReal(word: String) -> Bool {
+        // this method will make an instance of UITextChecker, which is responsible for scanning strings for misspelled words.
+        let checker = UITextChecker()
+        // We’ll then create an NSRange to scan the entire length of our string,
+        let range = NSRange(location: 0, length: word.utf16.count)
+        // then call rangeOfMisspelledWord() on our text checker so that it looks for wrong words.
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        // get back another NSRange telling us where the misspelled word was found, but if the word was OK the location for that range will be the special value NSNotFound.
+        return misspelledRange.location == NSNotFound
+    }
+
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 
     func startGame() {
@@ -59,7 +118,7 @@ struct ContentView: View {
                 // 3. Split the string up into an array of strings, splitting on line breaks
                 let allWords = startWords.components(separatedBy: "\n")
 
-                // 4. Pick one random word, or use "silkworm" as a sensible default
+                // 4. Pick one random word, or use "silkworm" as a sensible default (which will never be used)
                 rootWord = allWords.randomElement() ?? "silkworm"
 
                 // If we are here everything has worked, so we can exit
