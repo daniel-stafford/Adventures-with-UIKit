53c1765bfac246ec22a11e9d76e514c043f92baf
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-22 14:22:37 +0100

    Add explicit animation example

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,31 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        print(animationAmount)
-
-        return VStack {
-            // Stepp will fill in smooth grap between 1 to 2, 2 to 3, etc.
-            // These binding animations effectively turn the tables on implicit animations: rather than setting the animation on a view and implicitly animating it with a state change, we now set nothing on the view and explicitly animate it with a state change. In the former, the state change has no idea it will trigger an animation, and in the latter the view has no idea it will be animated – both work and both are important.
-            Stepper("Scale amount", value: $animationAmount.animation(
-                // no need to specific animation value to watch
-                .easeInOut(duration: 1)
-                    .repeatCount(3, autoreverses: true)
-            ), in: 1 ... 10)
-
-            Spacer()
-
-            Button("Tap Me") {
-                animationAmount += 1
+        Button("Explicit Animiation") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
             }
-            .padding(40)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount)
         }
+        .padding(100)
+        .background(.green)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        // explicit animation
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
