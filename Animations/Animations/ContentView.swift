5c6acb23df45e0880dedb36abfb884c38f6b2dc9
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-22 16:15:26 +0100

    Add showing and hiding view with transitions (akin to conditional render)

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,38 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    let letters = Array("Ciao SwiftUI")
-    @State private var enabled = false
-    // First, we need some state to store the amount of their drag:
-    @State private var dragAmount = CGSize.zero
-
+    @State private var isShowingRed = false
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0 ..< letters.count) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .foregroundColor(enabled ? .blue : .red)
-                    // SwiftUI gives us a dedicated modifier for this called offset(), which lets us adjust the X and Y coordinate of a view without moving other views around it. You can pass in discrete X and Y coordinates if you want to, but – by no mere coincidence – offset() can also take a CGSize directly.
-                    .offset(dragAmount)
-                    // add an implicit animation that will animate the drag and the release
-                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                // We can get SwiftUI’s default view transition by wrapping the state change using withAnimation(), like this:
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    // the app now fades the red rectangle in and out, while also moving the button up to make space. It looks OK, but we can do better with the transition() modifier.
+//                    .transition(.scale)
+                    // . A useful one is .asymmetric, which lets us use one transition when the view is being shown and another when it’s disappearing. 
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            // Drag gestures have two extra modifiers that are useful to us here: onChanged() lets us run a closure whenever the user moves their finger, and onEnded() lets us run a closure when the user lifts their finger off the screen, ending the drag.
-            DragGesture()
-                // For our onChanged() modifier we’re going to read the translation of the drag, which tells us how far it’s moved from the start point – we can assign that directly to dragAmount so that our view moves along with the gesture.
-                .onChanged { dragAmount = $0.translation }
-                // For onEnded() we’re going to ignore the input entirely, because we’ll be setting dragAmount back to zero.
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                    // explicit animiation
-//                    withAnimation(.spring()) {
-//                         dragAmount = .zero
-                }
-        )
     }
 }
 
