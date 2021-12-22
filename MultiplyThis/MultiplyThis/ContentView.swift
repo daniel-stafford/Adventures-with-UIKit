6384abb3df1f48b62fe61a283349f3def9010c45
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-22 22:15:48 +0100

    Set up app

---
+++ MultiplyThis/MultiplyThis/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplyThis
+//
+//  Created by Daniel Stafford on 12/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplyThis/MultiplyThis/MultiplyThisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyThisApp.swift
+//  MultiplyThis
+//
+//  Created by Daniel Stafford on 12/22/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyThisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
