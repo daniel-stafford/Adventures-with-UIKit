abf50c163e7a9a5868fb0e6819a47bc2a87d7374
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-22 17:15:02 +0100

    Add custom transition example

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,25 +7,49 @@
 
 import SwiftUI
 
+// creating a CornerRotateModifier struct that has an anchor point to control where the rotation should take place, and an amount to control how much rotation should be applied:
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            // rotationEffect() is similar to rotation3DEffect(), except it always rotates around the Z axis. However, it also gives us the ability to control the anchor point of the rotation – which part of the view should be fixed in place as the center of the rotation. SwiftUI gives us a UnitPoint type for controlling the anchor, which lets us specify an exact X/Y point for the rotation or use one of the many built-in options – .topLeading, .bottomTrailing, .center, and so on.
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            // The addition of clipped() there means that when the view rotates the parts that are lying outside its natural rectangle don’t get drawn.
+            .clipped()
+    }
+}
+
+// wrap that in an extension to AnyTransition, making it rotate from -90 to 0 on its top leading corner:
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingRed = false
+
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                // We can get SwiftUI’s default view transition by wrapping the state change using withAnimation(), like this:
-                withAnimation {
-                    isShowingRed.toggle()
-                }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
 
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    // the app now fades the red rectangle in and out, while also moving the button up to make space. It looks OK, but we can do better with the transition() modifier.
-//                    .transition(.scale)
-                    // . A useful one is .asymmetric, which lets us use one transition when the view is being shown and another when it’s disappearing. 
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
             }
         }
     }
