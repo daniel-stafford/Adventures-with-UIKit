21913e3b04ec344be19fe2ec90d7a4922aa9972c
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-22 15:22:37 +0100

    Add example of gesture animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,22 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Ciao SwiftUI")
     @State private var enabled = false
+    // First, we need some state to store the amount of their drag:
+    @State private var dragAmount = CGSize.zero
 
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        HStack(spacing: 0) {
+            ForEach(0 ..< letters.count) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .foregroundColor(enabled ? .blue : .red)
+                    // SwiftUI gives us a dedicated modifier for this called offset(), which lets us adjust the X and Y coordinate of a view without moving other views around it. You can pass in discrete X and Y coordinates if you want to, but – by no mere coincidence – offset() can also take a CGSize directly.
+                    .offset(dragAmount)
+                    // add an implicit animation that will animate the drag and the release
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        // important to fill background before clipShape 😁
-        .background(enabled ? .blue : .red)
-        //  For even more control, it’s possible to disable animations entirely by passing nil to the modifier. For example, you might want the color change to happen immediately but the clip shape to retain its animation, in which case you’d write this:
-        .animation(nil, value: enabled)
-//        .animation(.default, value: enabled)
-        .foregroundColor(.white)
-        // create another animiation to affect modifier after the previous animation
-        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
-        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+        .gesture(
+            // Drag gestures have two extra modifiers that are useful to us here: onChanged() lets us run a closure whenever the user moves their finger, and onEnded() lets us run a closure when the user lifts their finger off the screen, ending the drag.
+            DragGesture()
+                // For our onChanged() modifier we’re going to read the translation of the drag, which tells us how far it’s moved from the start point – we can assign that directly to dragAmount so that our view moves along with the gesture.
+                .onChanged { dragAmount = $0.translation }
+                // For onEnded() we’re going to ignore the input entirely, because we’ll be setting dragAmount back to zero.
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                    // explicit animiation
+//                    withAnimation(.spring()) {
+//                         dragAmount = .zero
+                }
+        )
     }
 }
 
