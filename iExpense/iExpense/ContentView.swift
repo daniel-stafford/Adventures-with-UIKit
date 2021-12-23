fef35405a69c64c350ed1433f0e450c8e4f0cdcb
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-23 21:46:15 +0100

    Add Archiving Swift objects with Codable

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,11 +7,27 @@
 
 import SwiftUI
 
+//Codable allows for archiving
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
     @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
     @AppStorage("tapCount") private var tapCount2 = 0
 
     var body: some View {
+        Button("Save User") {
+            //use endcoder for writing, decoder for getting
+            let encoder = JSONEncoder()
+            //writing the data direcctly rather than going through @AppStorage, which can only hold simple data
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
+        
         Button("Tap count: \(tapCount)") {
             tapCount += 1
             UserDefaults.standard.set(self.tapCount, forKey: "Tap")
