b59fc1134b4a1ff0f4856dc58d963e6a53bae873
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-25 21:55:45 +0100

    idd JSON decode example

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,16 +7,34 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(0 ..< 100) { row in
-                NavigationLink {
-                    Text("Detail \(row))")
-                } label: {
-                    Text("Row \(row)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
-            }.navigationTitle("Home")
+            }
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
     }
 }
