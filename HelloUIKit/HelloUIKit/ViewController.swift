7658f75b467daa8abd97668c21723b0b893b17c4
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-25 19:38:51 +0100

    Start app with simple Hello World

---
+++ HelloUIKit/HelloUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloUIKit
+//
+//  Created by Daniel Stafford on 12/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
