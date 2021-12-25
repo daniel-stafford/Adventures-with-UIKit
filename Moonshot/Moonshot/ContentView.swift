9a9d834b44cdf67bcab6d5bd83fcb2e72e57e1f7
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-25 18:39:27 +0100

    Merge

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0 {
+        didSet {
+            print("counter", counter)
+        }
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Up One"){
+        counter += 1
+        }
+        Button("Down") {
+            counter -= 1
+        }
+        Text("Counter\(counter)")
     }
 }
 
--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -27,6 +27,16 @@ struct GridStack<Content: View>: View {
         }
     }
 }
+//        Text("Hello, world!")
+        //in a VStack Swift uses TupleView, which contains two views inside) behind the scnes
+        //e.g.  hold two views -> public static func buildBlock<C0, C1>(_ c0: C0, _ c1: C1) -> TupleView<(C0, C1)> where C0 : View, C1 : View
+        // e.g. tuple that can hold 10 max ->  public static func buildBlock<C0, C1, C2, C3, C4, C5, C6, C7, C8, C9>(_ c0: C0, _ c1: C1, _ c2: C2, _ c3: C3, _ c4: C4, _ c5: C5, _ c6: C6, _ c7: C7, _ c8: C8, _ c9: C9) -> TupleView<(C0, C1, C2, C3, C4, C5, C6, C7, C8, C9)> where C0 : View, C1 : View, C2 : View, C3 : View, C4 : View, C5 : View, C6 : View, C7 : View, C8 : View, C9 : View
+        //why SwiftUI has a 10 item limit. 😎
+//        VStack {
+//            Text("Hello")
+//            Text("Hello")
+
+       //in the case of four text items, Swift uses ViewBuilder to create a view tuple (again, up to 10), so we're not sending back four things but rather one (tuple)
 
 struct ContentView: View {
     var body: some View {
