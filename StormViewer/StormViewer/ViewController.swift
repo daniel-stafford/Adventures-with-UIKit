0fc04afc437cb87a8675bab267c66caf8f1bdf34
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-25 16:18:35 +0100

    Add basic basic app

--- StormViewer/StormViewer/ViewController.swift
+++ StormViewer/StormViewer/ViewController.swift
@@ -5,32 +5,51 @@
 //  Created by Daniel Stafford on 12/24/21.
 //
 
-// this file will reference the iOS user interface toolkit.”
 import UIKit
 
-// “I want to create a new screen of data called ViewController, based on UIViewController.” When you see a data type that starts with “UI”, it means it comes from UIKit. UIViewController is Apple’s default screen type, which is empty and white until we change it.
-class ViewController: UIViewController {
-    //To create a property, you need to declare it outside of methods.
-    //That pictures array will be created when the ViewController screen is created, and exist for as long as the screen exists. It will be empty, because we haven’t actually filled it with anything, but at least it’s there ready for us to fill.
+class ViewController: UITableViewController {
     var pictures = [String]()
+    var selectedPictureNumber = 0
+    var totalPictures = 0
 
-    // the override keyword is needed because it means “we want to change Apple’s default behavior from UIViewController.” viewDidLoad() is called by UIKit when the screen has loaded, and is ready for you to customize.
     override func viewDidLoad() {
-        // This super call means “tell Apple’s UIViewController to run its own code before I run mine,” and you’ll see this used a lot.
         super.viewDidLoad()
-        // This is a data type that lets us work with the filesystem, and in our case we'll be using it to look for files.
+        
+        //set navigation bar title, may get cut off due to length of word and reverted to "Back"
+        title = "Storm Viewer"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
-        // declares a constant called path that is set to the resource path of our app's bundle. Remember, a bundle is a directory containing our compiled program and all our assets. So, this line says, "tell me where I can find all those images I added to my app."
         let path = Bundle.main.resourcePath!
-        // The items constant will be an array of strings containing filenames.
-        // In this instance it’s perfectly fine to use Bundle.main.resourcePath! and try!, because if this code fails it means our app can't read its own data so something must be seriously wrong. Some Swift developers attempt to write code to handle these catastrophic errors at runtime, but sadly all too often they just mask the actual problem that occurred.
         let items = try! fm.contentsOfDirectory(atPath: path)
-
+        
         for item in items {
             if item.hasPrefix("nssl") {
                 pictures.append(item)
             }
         }
-        print(pictures)
+        pictures.sort()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // 2: success! Set its selectedImage property
+            vc.selectedImage = pictures[indexPath.row]
+            vc.title = "Pictures \(indexPath.row + 1) of \(pictures.count)"
+            // 3: now push it onto the navigation controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
