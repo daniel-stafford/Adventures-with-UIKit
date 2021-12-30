ea47d4823f07727cac54ade0a68ffd6df45fce48
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-30 22:39:22 +0100

    Design layout with interface builder

---
+++ GuessTheFlagUIKit/GuessTheFlagUIKit/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  UIKitPreview
+//
+//  Created by Daniel Stafford on 12/30/21.
+//
+
+import UIKit
+import SwiftUI
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
+struct VCPreview: PreviewProvider {
+
+    static var previews: some View {
+        VCContainerView().edgesIgnoringSafeArea(.all)
+    }
+    struct VCContainerView: UIViewControllerRepresentable {
+        func makeUIViewController(context: Context) -> UIViewController {
+            return ViewController()
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+
+        typealias UIViewControllerType = UIViewController
+
+    }
+
+}
