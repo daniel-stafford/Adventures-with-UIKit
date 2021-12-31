4341e77b5d13c6e6ce74dd5cb3b76e4119a75e02
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2021-12-31 20:29:00 +0100

    Complete app with challenges

--- GuessTheFlagUIKit/GuessTheFlagUIKit/ViewController.swift
+++ GuessTheFlagUIKit/GuessTheFlagUIKit/ViewController.swift
@@ -13,47 +13,87 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     @IBOutlet var buttons: [UIButton]!
+    // not sure why this isn't appearing 😕
+    @IBOutlet var scoreText: UILabel!
+
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var turns = 0
+    var numQuestions = 5
 
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        if score == 0 { scoreText.text = "Your score: 0" }
         askQuestion()
     }
-    
-    //UIAlertAction! = nil, force unwrap and set to nil, allows askQuestion() to work without writing askQuestion(action:nil)
+
+    // UIAlertAction! = nil, force unwrap and set to nil, allows askQuestion() to work without writing askQuestion(action:nil)
     func askQuestion(action: UIAlertAction! = nil) {
+        turns += 1
         countries.shuffle()
         correctAnswer = Int.random(in: 0 ... 2)
-        
         getTitle()
-        // https://programmingwithswift.com/how-to-get-index-and-value-from-for-loop-with-swift/
         for (index, button) in buttons.enumerated() {
             button.layer.borderWidth = 1
             button.setImage(UIImage(named: countries[index]), for: .normal)
         }
     }
 
-    func getTitle(){
-        title = countries[correctAnswer].capitalized
-           if title == "Us" { title = "United States" }
-           if title == "Uk" { title = "United Kingdom" }
+    func formatCountry(country: String) -> String {
+        print("country to be formatted", country)
+        var result = ""
+        if country == "us" { result = "The United States" }
+        else if country == "uk" { result = "The United Kingdom" } else {
+            result = country.capitalized
+        }
+        return result
     }
-    
+
+    func getTitle() {
+        let country = formatCountry(country: countries[correctAnswer])
+        title = "\(country) (current score: \(score))"
+        
+    }
+
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
-        if sender.tag == correctAnswer + 1 {
+        if sender.tag - 1 == correctAnswer {
             score += 1
             title = "Correct"
         } else {
             score -= 1
-            title = "Wrong"
+            let tappedFlag = formatCountry(country: countries[sender.tag - 1])
+            title = "Wrong. That's the flag of \(tappedFlag)"
         }
-        
+        scoreText.text = "Your score: \(score)"
+        alertTurn(title: title)
+    }
+
+    func checkTurns(action: UIAlertAction! = nil) {
+        if turns == numQuestions {
+            alertSummary()
+        } else {
+            askQuestion()
+        }
+    }
+
+    func alertTurn(title: String) {
         let ac = UIAlertController(title: title, message: "Your score is now \(score)", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: ("Continue"), style: .default, handler: askQuestion))
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: checkTurns))
         present(ac, animated: true)
+    }
+
+    func alertSummary() {
+        let ac = UIAlertController(title: "Game Over", message: "That's it! Your score was \(score) out of \(numQuestions)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "New Game", style: .default, handler: resetGame))
+        present(ac, animated: true)
+    }
+
+    func resetGame(action: UIAlertAction! = nil) {
+        score = 0
+        turns = 0
+        askQuestion()
     }
 }
