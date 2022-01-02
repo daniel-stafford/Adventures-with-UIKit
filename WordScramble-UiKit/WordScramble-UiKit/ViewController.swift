fc62fd26509b87362a4b40b5e3e3b56efa2c9a6c
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-02 21:52:34 +0100

    Reading from disk: contentsOf

--- WordScramble-UiKit/WordScramble-UiKit/ViewController.swift
+++ WordScramble-UiKit/WordScramble-UiKit/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UITableViewController{
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+
+        // 👀 The reason we use isEmpty is because some collection types, such as string, have to calculate their size by counting over all the elements they contain, so reading count == 0 can be significantly slower than using isEmpty.
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        // reloadData() forces it to call numberOfRowsInSection again, as well as calling cellForRowAt repeatedly.
+        tableView.reloadData()
+    }
 }
-
