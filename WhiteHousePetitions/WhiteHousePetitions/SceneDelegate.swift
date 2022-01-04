ea38e651f3cbd544e17af5e34f8e268dd09f27aa
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-04 21:39:44 +0100

    Finishing touches: didFinishLaunchingWithOptions

--- WhiteHousePetitions/WhiteHousePetitions/SceneDelegate.swift
+++ WhiteHousePetitions/WhiteHousePetitions/SceneDelegate.swift
@@ -13,9 +13,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        if let tabBarController = window?.rootViewController as? UITabBarController {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "NavController")
+            vc.tabBarItem = UITabBarItem(tabBarSystemItem: .topRated, tag: 1)
+            tabBarController.viewControllers?.append(vc)
+        }
+        
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
--- WhiteHousePetitions/WhiteHousePetitions/ViewController.swift
+++ WhiteHousePetitions/WhiteHousePetitions/ViewController.swift
@@ -14,14 +14,28 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
 
         // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
 
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 // we're OK to parse!
                 parse(json: data)
+                return
             }
         }
+
+        showError()
+    }
+
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 
     func parse(json: Data) {
@@ -50,4 +64,6 @@ class ViewController: UITableViewController {
         vc.detailItem = petitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
+
+
 }
