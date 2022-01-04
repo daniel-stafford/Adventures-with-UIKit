66fed5af84374f61d899cd72cec310f8e838aa22
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-04 12:58:26 +0100

    Set up app

---
+++ WhiteHousePetitions/WhiteHousePetitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitions
+//
+//  Created by Daniel Stafford on 1/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
