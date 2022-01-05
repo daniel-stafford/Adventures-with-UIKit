0a14cdbdd206371debfbd08e0e1d05ecb490c60d
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-05 21:15:50 +0100

    Property observers: didSet

--- SwiftyWords/SwiftyWords/ViewController.swift
+++ SwiftyWords/SwiftyWords/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
     var activatedButtons = [UIButton]()
     var solutions = [String]()
 
-    var score = 0
+    var score = 0 {
+        didSet {
+            print("score updated")
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
 
     override func loadView() {
