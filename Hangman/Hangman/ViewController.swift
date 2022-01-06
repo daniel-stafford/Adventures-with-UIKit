c7adb7313e03c8593692e7df0ce0782144d28960
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-06 18:50:21 +0100

    Start app

---
+++ Hangman/Hangman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Daniel Stafford on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
