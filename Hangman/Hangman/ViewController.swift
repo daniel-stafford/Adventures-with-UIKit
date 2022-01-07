372cea82331a7fbc9f98034113057247f7e2cb7b
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-07 10:33:25 +0100

    Add mistake alert and masked answer functionality

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -9,53 +9,60 @@ import UIKit
 
 extension Array where Element: Equatable {
     func indexes(of element: Element) -> [Int] {
-        return self.enumerated().filter({ element == $0.element }).map({ $0.offset })
+        return enumerated().filter({ element == $0.element }).map({ $0.offset })
     }
 }
 
 class ViewController: UIViewController {
-    var answer = "Apple"
-    var turnsLeft = 7 {
+    var mistakesLeft = 7 {
         didSet {
-            print(turnsLeft, "turnsLeft")
+            mistakesLeftLabel.text = "Mistakes Remaining: \(mistakesLeft)"
         }
     }
 
+    var answer = [String]()
+    var maskedAnswer = [String]()
     var titleLabel: UILabel!
     var letterButton: UILabel!
     var letterButtons = [UIButton]()
     var answerLabel: UILabel!
-    var turnsLeftLabel: UILabel!
+    var mistakesLeftLabel: UILabel!
 
     override func loadView() {
         view = UIView()
-
         view.backgroundColor = UIColor.systemBackground
+
+        // get 1000 words from API
+        // store words in an array, shuffle it
+        // pop the last one, take string, turn into array, set to answer
+
+        answer = ["A", "P", "P", "L", "E"]
+
+        createAnswerLabelText(answer)
 
         titleLabel = UILabel()
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.text = "Hangman"
         titleLabel.textColor = .none
-        titleLabel.font = UIFont.systemFont(ofSize: 50)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 40)
         view.addSubview(titleLabel)
 
         answerLabel = UILabel()
         answerLabel.translatesAutoresizingMaskIntoConstraints = false
         answerLabel.font = UIFont.systemFont(ofSize: 40)
         answerLabel.textColor = .none
-        answerLabel.text = createMaskedAnswer(answer)
+        answerLabel.text = maskedAnswer.joined(separator: "")
         view.addSubview(answerLabel)
 
-        turnsLeftLabel = UILabel()
-        turnsLeftLabel.translatesAutoresizingMaskIntoConstraints = false
-        turnsLeftLabel.font = UIFont.systemFont(ofSize: 25)
-        turnsLeftLabel.textColor = .none
-        turnsLeftLabel.text = "Mistakes Remaining: \(turnsLeft)"
-        view.addSubview(turnsLeftLabel)
+        mistakesLeftLabel = UILabel()
+        mistakesLeftLabel.translatesAutoresizingMaskIntoConstraints = false
+        mistakesLeftLabel.font = UIFont.systemFont(ofSize: 25)
+        mistakesLeftLabel.textColor = .none
+        mistakesLeftLabel.text = "Mistakes remaining: \(mistakesLeft)"
+        view.addSubview(mistakesLeftLabel)
 
         let buttonsView = UIView()
         buttonsView.translatesAutoresizingMaskIntoConstraints = false
-//        buttonsView.layer.borderWidth = 1
         buttonsView.layer.borderColor = UIColor.gray.cgColor
         view.addSubview(buttonsView)
 
@@ -81,7 +88,6 @@ class ViewController: UIViewController {
         for (i, char) in alphabet.uppercased().enumerated() {
             letterButtons[i].setTitle(String(char), for: .normal)
         }
-        print(letterButtons.count)
 
         // hide and disable buttons beyond the 26 letter alphabet
         for i in 26 ..< 28 {
@@ -96,9 +102,8 @@ class ViewController: UIViewController {
             answerLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 150),
             answerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 
-            turnsLeftLabel.topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 150),
-
-            turnsLeftLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mistakesLeftLabel.topAnchor.constraint(equalTo: answerLabel.bottomAnchor, constant: 150),
+            mistakesLeftLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
 
             buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20),
             buttonsView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
@@ -109,19 +114,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @objc func letterTapped(_ sender: UIButton) {
         guard let guessedLetter = sender.titleLabel?.text else { return }
-        print("guessLetter", guessedLetter)
+
+        if answer.contains(guessedLetter) {
+            let foundIndices = answer.indexes(of: guessedLetter)
+
+            for (i, _) in maskedAnswer.enumerated() {
+                if foundIndices.contains(i) {
+                    maskedAnswer[i] = guessedLetter
+                    answerLabel.text = maskedAnswer.joined(separator: "")
+                }
+            }
+        } else {
+            mistakesLeft -= 1
+            if mistakesLeft == 0 {
+                let ac = UIAlertController(title: "You lose!", message: "Go ahead and try again.", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+                // reset game
+            }
+        }
+        sender.isHidden = true
     }
 
-    func createMaskedAnswer(_ str: String) -> String {
-        var maskedAnswer = [String]()
-        for _ in str {
+    func correctAnswer() {
+        print("Noice!")
+    }
+
+    func createAnswerLabelText(_ anwswer: [String]) {
+        for _ in answer {
             maskedAnswer.append("?")
         }
-        return maskedAnswer.joined(separator: "")
     }
 }
