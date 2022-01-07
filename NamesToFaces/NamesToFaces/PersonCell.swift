7ab3244600165c3b5291bf7e6559f442d72e2474
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-07 22:43:04 +0100

    Designing UICollectionView cells

--- NamesToFaces/NamesToFaces/PersonCell.swift
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -8,6 +8,6 @@
 import UIKit
 
 class PersonCell: UICollectionViewCell {
-    @IBOutlet var ImageView: UIImageView!
-    @IBOutlet var Name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
 }
