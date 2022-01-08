5d6a131f35a56f29b259138279fa7d3f09347355
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-08 17:09:13 +0100

    Scores on the board: SKLabelNode

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -9,6 +9,26 @@ import SpriteKit
 
 // allow us to use SKPhysicsContactDelegate
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+
+    var editLabel: SKLabelNode!
+
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
+
     override func didMove(to view: SKView) {
         // if you want to place an image in your game, the class to use is called SKSpriteNode, and it can load any picture from your app bundle just like UIImage.
         let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -30,13 +50,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
         makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
         makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
-        
+
         makeBouncer(at: CGPoint(x: 0, y: 0))
         makeBouncer(at: CGPoint(x: 256, y: 0))
         makeBouncer(at: CGPoint(x: 512, y: 0))
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
 
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        // top right of screen
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        // top left of screen
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
     }
 
     // user touches the screen
@@ -46,20 +78,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             // where did the touch happen in the whole of my game scene
             let location = touch.location(in: self)
 
-            let ball = SKSpriteNode(imageNamed: "ballRed")
-            // circueOfRadius - will behave as ball rather than square
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-            // 0 to 1 (not bouncey to super boundey
-            // note that physicsBody is optional (even though we just created it)
-            ball.physicsBody?.restitution = 0.4
-            // collisionBitMask = which things should I bump into? By default, set to everything
-            // contactTestBitMask = which colliosions do you want to know about? by default set to nothing
-            // so, bounce off everything and tell us all about each bounce (OK for simple games)
-            ball.physicsBody!.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-            ball.position = location
-            // name for tracking
-            ball.name = "ball"
-            addChild(ball)
+            // what nodes exist at the touch location in our scene
+            let objects = nodes(at: location)
+
+            // did the user tap the edit button?
+            if objects.contains(editLabel) {
+                // if so, toggle it
+                editingMode.toggle()
+            } else {
+                if editingMode {
+                    // edit is true
+                    // create a barrier box
+                    // random size
+                    let size = CGSize(width: Int.random(in: 16 ... 128), height: 16)
+                    // random color
+                    let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0 ... 1), green: CGFloat.random(in: 0 ... 1), blue: CGFloat.random(in: 0 ... 1), alpha: 1), size: size)
+                    // random rotation
+                    box.zRotation = CGFloat.random(in: 0 ... 3)
+                    box.position = location
+                    box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                    box.physicsBody?.isDynamic = false
+                    addChild(box)
+                } else {
+                    // edit is false
+                    // create a ball
+                    let ball = SKSpriteNode(imageNamed: "ballRed")
+                    // circueOfRadius - will behave as ball rather than square
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                    // 0 to 1 (not bouncey to super boundey
+                    // note that physicsBody is optional (even though we just created it)
+                    ball.physicsBody?.restitution = 0.4
+                    // collisionBitMask = which things should I bump into? By default, set to everything
+                    // contactTestBitMask = which colliosions do you want to know about? by default set to nothing
+                    // so, bounce off everything and tell us all about each bounce (OK for simple games)
+                    ball.physicsBody!.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                    // balls from top of screen
+                    ball.position = CGPoint(x: location.x, y: 700)
+                    // name for tracking
+                    ball.name = "ball"
+                    addChild(ball)
+                }
+            }
         }
     }
 
@@ -109,8 +168,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collisionBetween(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
 
@@ -123,11 +184,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // double collision may occur (ball hits slot, slot hits ball), so this prevents running twice
         guard let nodeA = contact.bodyA.node else { return }
         guard let nodeB = contact.bodyB.node else { return }
- 
+
         // first node is ball
         if nodeA.name == "ball" {
             collisionBetween(ball: nodeA, object: nodeB)
-        // second node is ball
+            // second node is ball
         } else if nodeB.name == "ball" {
             collisionBetween(ball: nodeB, object: nodeA)
         }
