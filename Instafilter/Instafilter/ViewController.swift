bac14266e7124755516898cced7823b99ac6e29d
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-09 23:10:36 +0100

    Add IB outlets and actions

--- Instafilter/Instafilter/ViewController.swift
+++ Instafilter/Instafilter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBOutlet var intensityChanged: NSLayoutConstraint!
+    @IBAction func save(_ sender: Any) {
+    }
+    @IBAction func changeFilter(_ sender: Any) {
+    }
 }
 
