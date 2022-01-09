6f8ba9eb86c2e183893ddd5db70b51fa5a74b188
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-09 13:47:00 +0100

    Start new app

---
+++ CameraAlbum/CameraAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraAlbum
+//
+//  Created by Daniel Stafford on 1/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
