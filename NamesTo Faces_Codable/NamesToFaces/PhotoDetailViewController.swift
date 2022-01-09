5b5c5d8694d5077e2ad69fb7da7b236780b4e8c5
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-09 19:10:20 +0100

    Rename folder

---
+++ NamesTo Faces_Codable/NamesToFaces/PhotoDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoDetailViewController.swift
+//  NamesToFaces
+//
+//  Created by Daniel Stafford on 1/9/22.
+//  Copyright © 2022 Shubham shah. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
