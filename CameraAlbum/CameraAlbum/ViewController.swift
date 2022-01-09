f86a82a630a7b3ce9c91ff3c4b864c18fc11bafb
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-09 16:37:50 +0100

    Set up layout

--- CameraAlbum/CameraAlbum/ViewController.swift
+++ CameraAlbum/CameraAlbum/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Photo", for: indexPath) as? PhotoCell else {
+            // we failed to get a PersonCell – bail out!
+            fatalError("Unable to dequeue PersonCell.")
+        }
 
-
+        // if we're still here it means we got a PersonCell, so we can return it
+        return cell
+    }
 }
-
