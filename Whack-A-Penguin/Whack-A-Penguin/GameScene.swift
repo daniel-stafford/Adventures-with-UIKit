dd80b7700a5940149dc205b25b9c48b631b1ee16
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-10 16:13:02 +0100

    Setting up

---
+++ Whack-A-Penguin/Whack-A-Penguin/GameScene.swift
@@ -0,0 +1,9 @@
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
