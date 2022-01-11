29f4f24417df12d93a0e8f44625012b317c6bd34
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-11 14:03:44 +0100

    Preparing for action

--- Animation-UIKit/Animation-UIKit/ViewController.swift
+++ Animation-UIKit/Animation-UIKit/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        currentAnimation += 1
 
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
 }
-
