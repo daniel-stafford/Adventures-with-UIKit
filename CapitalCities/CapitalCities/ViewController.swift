0054c01e833d31b09b26f3a205b1b982df781d2e
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-12 21:31:41 +0100

    Setting up

---
+++ CapitalCities/CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Daniel Stafford on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
