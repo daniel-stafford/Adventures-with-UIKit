2341de391ddc80af8fcc922ba7b89fb635b1d29a
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-14 12:30:47 +0100

    Setting up

---
+++ SpaceRace/SpaceRace/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  SpaceRace
+//
+//  Created by Daniel Stafford on 1/14/22.
+//
+
+import GameplayKit
+import SpriteKit
+
+class GameScene: SKScene {
+    private var label: SKLabelNode?
+    private var spinnyNode: SKShapeNode?
+
+    override func didMove(to view: SKView) {
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+    }
+}
