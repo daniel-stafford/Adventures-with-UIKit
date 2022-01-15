c95a639ddf5daebbffa76be355ebb98ccbb05ad8
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-15 23:46:01 +0100

    Debugging with assert()

--- Debug/Debug/ViewController.swift
+++ Debug/Debug/ViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         print(1, 2, 3, 4, 5, separator: "-")
+        // asserts are not used in release builds, only written during test builds
         print("Some message", terminator: "")
-
+        assert(1 == 1, "Maths failure!")
+//        assert(1 == 2, "Maths failure!")
+        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
     }
-
-
+    
+    func myReallySlowMethod() -> Bool {
+        return false
+    }
 }
-
