899ebca23783da3f1a1b5321fc961eca4107c248
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-15 23:21:18 +0100

    Basic Swift debugging using print()

--- Debug/Debug/ViewController.swift
+++ Debug/Debug/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(1, 2, 3, 4, 5, separator: "-")
+        print("Some message", terminator: "")
+
     }
 
 
