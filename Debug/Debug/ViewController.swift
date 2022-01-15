0cd83df39a2aab642b23c15bff5d933a460f1281
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-15 23:16:33 +0100

    Setting up

---
+++ Debug/Debug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Debug
+//
+//  Created by Daniel Stafford on 1/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
