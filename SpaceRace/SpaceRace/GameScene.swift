e4385279a38ed375f28aadb55e85d82d7823f055
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-15 20:36:11 +0100

    Bring on the enemies: Timer, linearDamping, angularDamping

--- SpaceRace/SpaceRace/GameScene.swift
+++ SpaceRace/SpaceRace/GameScene.swift
@@ -19,13 +19,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
 
+    // all possible things to throw at the player
+    let possibleEnemies = ["ball", "hammer", "tv"]
+    // note the new type Timer
+    var gameTimer: Timer?
+    var isGameOver = false
+
     override func didMove(to view: SKView) {
         backgroundColor = .black
 
         starfield = SKEmitterNode(fileNamed: "starfield")!
-        //right end, halfway up
+        // right end, halfway up
         starfield.position = CGPoint(x: 1024, y: 384)
-        //prefill screen with starfield (move 10 seconds worth of starts)
+        // prefill screen with starfield (move 10 seconds worth of starts)
         starfield.advanceSimulationTime(10)
         addChild(starfield)
         starfield.zPosition = -1
@@ -44,16 +50,51 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.position = CGPoint(x: 16, y: 16)
         scoreLabel.horizontalAlignmentMode = .left
         addChild(scoreLabel)
-        
+
         // will trigger our didSet property observor
         score = 0
 
-       // graivtiy is zero as well as in sapce
+        // graivtiy is zero as well as in sapce
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         // tell us when contacts happen
         physicsWorld.contactDelegate = self
+
+        // create our timer, new enemy every three seconds
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+    }
+
+    @objc func createEnemy() {
+        // else should never occur but randomElement requires it.
+        guard let enemy = possibleEnemies.randomElement() else { return }
+
+        let sprite = SKSpriteNode(imageNamed: enemy)
+        // place at random vertical position on right side of screen
+        sprite.position = CGPoint(x: 1200, y: Int.random(in: 50 ... 736))
+        addChild(sprite)
+
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        // allow to colldie with player
+        sprite.physicsBody?.categoryBitMask = 1
+        // moving hard to the left at constant rate
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        // constant spin as moving
+        sprite.physicsBody?.angularVelocity = 5
+        // no slow down over time
+        sprite.physicsBody?.linearDamping = 0
+        // no spin slow down over time
+        sprite.physicsBody?.angularDamping = 0
     }
 
     override func update(_ currentTime: TimeInterval) {
+        for node in children {
+            // if node is off screen, remove it
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+
+        if !isGameOver {
+            score += 1
+        }
     }
 }
