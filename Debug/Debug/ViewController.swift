7c62a3cf2d4b6cc7826f1f9afb5edbe22dfb7d2e
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-16 10:03:15 +0100

    Debugging with breakpoints

--- Debug/Debug/ViewController.swift
+++ Debug/Debug/ViewController.swift
@@ -11,14 +11,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(1, 2, 3, 4, 5, separator: "-")
-        // asserts are not used in release builds, only written during test builds
+        // asserts are not used in release builds
         print("Some message", terminator: "")
         assert(1 == 1, "Maths failure!")
 //        assert(1 == 2, "Maths failure!")
-        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
-    }
-    
-    func myReallySlowMethod() -> Bool {
-        return false
+//        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing!")
+//    }
+        for i in 1 ... 100 {
+            print(i)
+        }
+
+        func myReallySlowMethod() -> Bool {
+            return false
+        }
     }
 }
