17c6cd27534b16caf39d758599377998c4cee85f
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-17 11:32:42 +0100

    Establishing communication

--- JSInjection/Extension/Action.js
+++ JSInjection/Extension/Action.js
@@ -2,8 +2,9 @@ var Action = function() {};
 
 Action.prototype = {
 
+// tell iOS the JavaScript has finished preprocessing, and give this data dictionary to the extension.
 run: function(parameters) {
-
+    parameters.completionFunction({"URL": document.URL, "title": document.title });
 },
 
 finalize: function(parameters) {
--- JSInjection/Extension/ActionViewController.swift
+++ JSInjection/Extension/ActionViewController.swift
@@ -20,8 +20,13 @@ class ActionViewController: UIViewController {
             // gives the us first items info via closure
             // will contain a dict and error
             if let itemProvider = inputItem.attachments?.first {
-                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] _, _ in
-                    // do stuff!
+                itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] dict, _ in
+                    // NSDictionary works like a Swift dictionary, except you don't need to declare or even know what data types it holds. One of the nasty things about NSDictionary is that you don't need to declare or even know what data types it holds.
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    // Right now, there's nothing in that dictionary other than the data we sent from JavaScript, and that's stored in a special key called NSExtensionJavaScriptPreprocessingResultsKey. So, we pull that value out from the dictionary, and put it into a value called javaScriptValues.
+                    // We sent a dictionary of data from JavaScript, so we typecast javaScriptValues as an NSDictionary again so that we can pull out values using keys,
+                    guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    print(javaScriptValues)
                 }
             }
         }
