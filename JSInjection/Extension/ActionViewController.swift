3d8232026b0fe9e1320e9788bb1c17ffc4f293d3
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-17 13:38:27 +0100

    Fixing the keyboard: NotificationCenter

--- JSInjection/Extension/ActionViewController.swift
+++ JSInjection/Extension/ActionViewController.swift
@@ -16,7 +16,16 @@ class ActionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+       
+        // access notification center
+        let notificationCenter = NotificationCenter.default
+        // we care about hiding and changing frames of keyboard
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        
+        
         //  we only care about first input item created by the extension
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             // gives the us first items info via closure
@@ -48,5 +57,32 @@ class ActionViewController: UIViewController {
         item.attachments = [customJavaScript]
 
         extensionContext?.completeRequest(returningItems: [item])
+    }
+    
+    // will receive a parameter that is of type Notification. This will include the name of the notification as well as a Dictionary containing notification-specific information called userInfo.
+    @objc func adjustForKeyboard(notification: Notification) {
+        // When working with keyboards, the dictionary will contain a key called UIResponder.keyboardFrameEndUserInfoKey telling us the frame of the keyboard after it has finished animating. This will be of type NSValue, which in turn is of type CGRect. The CGRect struct holds both a CGPoint and a CGSize, so it can be used to describe a rectangle.
+        // we cast as NSValue because a quirck of Objective-C was that arrays and dictionaries couldn't contain structures like CGRect, so Apple had a special class called NSValue that acted as a wrapper around structures so they could be put into dictionaries and arrays.
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        // pull out the correct frame of the keyboard
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        //  we need to convert the rectangle to our view's co-ordinates. This is because rotation isn't factored into the frame, so if the user is in landscape we'll have the width and height flipped – using the convert() method will fix that.
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+       
+        // workaround for hardware keyboards being connected by explicitly setting the insets to be zero.
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            // The next thing we need to do in the adjustForKeyboard() method is to adjust the contentInset and scrollIndicatorInsets of our text view. These two essentially indent the edges of our text view so that it appears to occupy less space even though its constraints are still edge to edge in the view.
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        // Finally, we're going to make the text view scroll so that the text entry cursor is visible. If the text view has shrunk this will now be off screen, so scrolling to find it again keeps the user experience intact.
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
     }
 }
