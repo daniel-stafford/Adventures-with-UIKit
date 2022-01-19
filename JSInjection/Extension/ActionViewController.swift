5247fe7f324a3e3eab23c27d504e68f95a706da9
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-19 14:30:09 +0100

    Add prewritten scripts

--- JSInjection/Extension/ActionViewController.swift
+++ JSInjection/Extension/ActionViewController.swift
@@ -16,16 +16,17 @@ class ActionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
-       
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(chooseScript))
+
         // access notification center
         let notificationCenter = NotificationCenter.default
         // we care about hiding and changing frames of keyboard
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-        
-        
+
         //  we only care about first input item created by the extension
         if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
             // gives the us first items info via closure
@@ -49,16 +50,30 @@ class ActionViewController: UIViewController {
         }
     }
 
+    @objc func chooseScript() {
+        let ac = UIAlertController(title: "Choose Script", message: "", preferredStyle: .alert)
+        ac.addAction(generateAlertAction(title: "Display webpage title", text: "alert(document.title);"))
+        ac.addAction(generateAlertAction(title: "Display current year", text: "const d = new Date; alert(d.getFullYear());"))
+        ac.addAction(generateAlertAction(title: "Display user agent info", text: "alert(navigator.userAgent);"))
+        present(ac, animated: true)
+    }
+
+    func generateAlertAction(title: String, text: String) -> UIAlertAction {
+        return UIAlertAction(title: title, style: .default) { [weak self] _ in
+            self?.script.text = text
+        }
+    }
+
     @IBAction func done() {
         let item = NSExtensionItem()
-        let argument: NSDictionary = ["customJavaScript": script.text]
+        let argument: NSDictionary = ["customJavaScript": script.text!]
         let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
         let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
         item.attachments = [customJavaScript]
 
         extensionContext?.completeRequest(returningItems: [item])
     }
-    
+
     // will receive a parameter that is of type Notification. This will include the name of the notification as well as a Dictionary containing notification-specific information called userInfo.
     @objc func adjustForKeyboard(notification: Notification) {
         // When working with keyboards, the dictionary will contain a key called UIResponder.keyboardFrameEndUserInfoKey telling us the frame of the keyboard after it has finished animating. This will be of type NSValue, which in turn is of type CGRect. The CGRect struct holds both a CGPoint and a CGSize, so it can be used to describe a rectangle.
@@ -70,7 +85,6 @@ class ActionViewController: UIViewController {
         //  we need to convert the rectangle to our view's co-ordinates. This is because rotation isn't factored into the frame, so if the user is in landscape we'll have the width and height flipped – using the convert() method will fix that.
         let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
 
-       
         // workaround for hardware keyboards being connected by explicitly setting the insets to be zero.
         if notification.name == UIResponder.keyboardWillHideNotification {
             script.contentInset = .zero
@@ -81,7 +95,7 @@ class ActionViewController: UIViewController {
 
         // Finally, we're going to make the text view scroll so that the text entry cursor is visible. If the text view has shrunk this will now be off screen, so scrolling to find it again keeps the user experience intact.
         script.scrollIndicatorInsets = script.contentInset
-        
+
         let selectedRange = script.selectedRange
         script.scrollRangeToVisible(selectedRange)
     }
