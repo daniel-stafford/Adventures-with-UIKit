393cead7cef05a35052bc42b3dc25e052d95ddf7
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-19 17:10:14 +0100

    Setting up

---
+++ Fireworks/Fireworks/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Fireworks
+//
+//  Created by Daniel Stafford on 1/19/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView){
+        
+    }
+    
+}
