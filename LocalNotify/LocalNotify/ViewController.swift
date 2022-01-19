d34bcb064edcd901d9edd17086ebf49b5b72360e
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-19 20:00:45 +0100

    Scheduling notifications: UNUserNotificationCenter and UNNotificationRequest

--- LocalNotify/LocalNotify/ViewController.swift
+++ LocalNotify/LocalNotify/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+// new framework for user nitifications
+import UserNotifications
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
 
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
 
+        // show message, badge on icon, play a sound
+        // to delete permissions, go to hardware menu, and erase all content and settings
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, _ in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh")
+            }
+        }
+    }
+
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        //only show new notifications
+        center.removeAllPendingNotificationRequests()
+        // create content
+        let content = UNMutableNotificationContent()
+        // big text, just a couple of words at most
+        content.title = "Late wake up call"
+        // main text for notification, e.g. actual content of text message
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        // custom action
+        content.categoryIdentifier = "alarm"
+        // custom data to attach
+        content.userInfo = ["customData": "fizzbuzz"]
+        // specify sound or use default
+        content.sound = UNNotificationSound.default
+
+        // create calendar notification
+        var dateComponents = DateComponents()
+        // 10:30
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+    
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        // can use whatever identifier, but must be unique
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
 }
-
