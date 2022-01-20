30122758fc501b8e2b77a3a99e94c1cb33c8d579
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-20 20:03:24 +0100

    Setting up

---
+++ Detect-a-Beacon/Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detect-a-Beacon
+//
+//  Created by Daniel Stafford on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
