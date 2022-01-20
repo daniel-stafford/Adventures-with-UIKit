8f329185afb54807a897b722e24ac4ab7a8ddd14
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-20 22:47:43 +0100

    Basics quick start: SKShapeNode

--- SwiftyNinja/SwiftyNinja/GameScene.swift
+++ SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -8,6 +8,68 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+
+        createScore()
+        createLives()
+        createSlices()
+    }
+
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+    }
+
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+
+            livesImages.append(spriteNode)
+        }
+    }
+
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
     }
 }
