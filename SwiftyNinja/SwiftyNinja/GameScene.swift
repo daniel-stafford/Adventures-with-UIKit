8232bced976abe7021419b20c97eb43d3e0147ae
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-20 22:43:05 +0100

    Setting up

---
+++ SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  SwiftyNinja
+//
+//  Created by Daniel Stafford on 1/20/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    }
+}
