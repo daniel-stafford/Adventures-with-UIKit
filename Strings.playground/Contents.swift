40ad551687edb89f5e66e4435498dbc8b2e36d3d
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 20:15:46 +0100

    Create times extension

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -170,5 +170,17 @@ UIView.animate(
 
 //Extend Int with a times() method that runs a closure as many times as the number is high. For example, 5.times { print("Hello!") } will print “Hello” five times.
 
+extension Int {
+    func times(_ method: () -> Void)  {
+        for _ in 0 ..< self {
+            method()
+        }
+    }
+}
+
+5.times {
+    print("hello")
+}
+
 
 //Extend Array so that it has a mutating remove(item:) method. If the item exists more than once, it should remove only the first instance it finds. Tip: you will need to add the Comparable constraint to make this work!
