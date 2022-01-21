92d294b430f5d46095b9d2914b02fbc60b4c8a53
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 13:38:16 +0100

    Strings are not arrays

--- SwiftStrings.playground/Contents.swift
+++ SwiftStrings.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let name = "Taylor"
+
+for letter in name {
+    print("Give me a \(letter)!")
+}
+
+// wont work
+//print(name[3])
+
+
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+// With that in place, we can now read name[3] just fine. However, it creates the possibility that someone might write code that loops over a string reading individual letters, which they might not realize creates a loop within a loop and has the potential to be slow.
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
+print(letter2)
+//name.count is long
+//better to use name.isEmpty much quicker
