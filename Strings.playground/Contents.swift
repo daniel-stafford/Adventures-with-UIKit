a46bf9e3092f2b4ce0c2ee28f8273cc6f6d91c22
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 18:11:33 +0100

    Setting up

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -143,3 +143,10 @@ extension String {
 
 "this\nis\na\ntest".lines()
 
+//Extend UIView so that it has a bounceOut(duration:) method that uses animation to scale its size down to 0.0001 over a specified number of seconds.
+
+
+//Extend Int with a times() method that runs a closure as many times as the number is high. For example, 5.times { print("Hello!") } will print “Hello” five times.
+
+
+//Extend Array so that it has a mutating remove(item:) method. If the item exists more than once, it should remove only the first instance it finds. Tip: you will need to add the Comparable constraint to make this work!
