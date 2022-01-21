97f0f840bc55067f164cf113aba144b634c11b0a
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 16:35:25 +0100

    Working with strings in Swift

--- SwiftStrings.playground/Contents.swift
+++ SwiftStrings.playground/Contents.swift
@@ -23,3 +23,63 @@ let letter2 = name[3]
 print(letter2)
 //name.count is long
 //better to use name.isEmpty much quicker
+
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("345")
+
+extension String {
+    // remove a prefix if it exists
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        //dropFirst = remove certain number of letters from start of string, return substring
+        return String(self.dropFirst(prefix.count))
+    }
+
+    // remove a suffix if it exists
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        //dropLast = remove certain number of letters from end of string, return substring
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+password.deletingPrefix("123")
+password.deletingSuffix("45")
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        // check if string is empty (has no first letter)
+        guard let firstLetter = self.first else { return "" }
+        // first letter = string.element alias of characterbib, uppercase() = string
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+// check if any item in array is in input string
+// this works but isn't elegant
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+// arrays have second contains(where: ), add close input,contains, which cycles through array
+languages.contains(where: input.contains)
