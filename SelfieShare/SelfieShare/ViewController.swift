a4aa6264d299dbf4f9d7eb1f3593cf27a4ed90c1
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 21:28:28 +0100

    Importing photos again

--- SelfieShare/SelfieShare/ViewController.swift
+++ SelfieShare/SelfieShare/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController,  UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+
+        return cell
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        dismiss(animated: true)
+
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+    }
 
 }
 
