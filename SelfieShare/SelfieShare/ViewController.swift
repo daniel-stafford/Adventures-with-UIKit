6c334ae59552525388ff2b95ab51dd13e572acf3
daniel-stafford <33048490+daniel-stafford@users.noreply.github.com>
2022-01-21 21:04:51 +0100

    Setting up

---
+++ SelfieShare/SelfieShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfieShare
+//
+//  Created by Daniel Stafford on 1/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
